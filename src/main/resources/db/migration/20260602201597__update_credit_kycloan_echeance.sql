994db451d4a8929f5b06c59156cdb3dbc3965da6
Youssef222003 <mellouli.youssef11@gmail.com>
2026-02-08 15:03:06 +0100

    migration problem

--- src/main/resources/db/migration/20260602201597__update_credit_kycloan_echeance.sql
+++ src/main/resources/db/migration/20260602201597__update_credit_kycloan_echeance.sql
@@ -11,64 +11,63 @@
 -- Table: credit
 -- ========================
 -- Ajouter start_date, end_date, dependents
-/*
+
 ALTER TABLE credit
 ADD COLUMN start_date DATE NOT NULL,
 ADD COLUMN end_date DATE NOT NULL,
-ADD COLUMN dependents INT;
-*/
+ADD COLUMN dependents INT NOT NULL;
+
 
 -- Supprimer risk_score
-/*
+
 ALTER TABLE credit
 DROP COLUMN risk_score;
 ALTER TABLE credit
 DROP COLUMN approved_at;
-*/
+
 
 -- Modifier status pour ENUM
-/*
+
 ALTER TABLE credit
     MODIFY COLUMN status ENUM('PENDING','APPROVED','REJECTED','CANCELLED') NOT NULL DEFAULT 'PENDING';
-*/
 
 -- ========================
 -- Table: echeance
 -- ========================
 -- Modifier status pour ENUM
-/*
+
 ALTER TABLE echeance
     MODIFY COLUMN status ENUM('PENDING','PAID','OVERDUE','PARTIALLY_PAID') NOT NULL DEFAULT 'PENDING';
 
 ALTER TABLE echeance
 DROP COLUMN created_at;
-*/
+
 
 -- ========================
 -- Table: kyc_loan
 -- ========================
 -- Supprimer uploaded_at
-/*
+
 ALTER TABLE kyc_loan
 DROP COLUMN uploaded_at;
-*/
+
 
 -- Ajouter user_id et verified_status
-/*
+
 ALTER TABLE kyc_loan
 ADD COLUMN submitted_at NOT NULL,
 ADD COLUMN user_id BIGINT NOT NULL,
 ADD COLUMN verified_status ENUM('PENDING','APPROVED','REJECTED') NOT NULL DEFAULT 'PENDING';
-*/
+
 
 -- Ajouter index pour user_id
-/*
+
 CREATE INDEX idx_kyc_loan_user ON kyc_loan(user_id);
-*/
+
 
 -- Ajouter clé étrangère user_id
-/*
+
 ALTER TABLE kyc_loan
     ADD CONSTRAINT fk_kyc_loan_user
         FOREIGN KEY (user_id) REFERENCES user(user_id) ON DELETE CASCADE;
-*/
+
