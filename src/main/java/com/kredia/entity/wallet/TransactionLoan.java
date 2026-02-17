8880db4e603b729466303b7638228ce5cc0443b0
ayedmohamedazi <ayed.mohamedaziz.1@esprit.tn>
2026-02-17 15:26:50 +0100

    creation of TransactionLoan for echance ( on team request)

---
+++ src/main/java/com/kredia/entity/wallet/TransactionLoan.java
@@ -0,0 +1,27 @@
+package com.kredia.entity.wallet;
+import com.kredia.entity.credit.Echeance;
+
+import com.kredia.entity.credit.Echeance;
+import com.kredia.enums.TransactionStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "transaction")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TransactionLoan extends Transaction {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "echeance_id")
+    private Echeance echnace_id;
+
+
+}
