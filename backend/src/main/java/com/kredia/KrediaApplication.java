fab47d5acf82802c6a4eb52caf0708c34c01e555
Youssef222003 <mellouli.youssef11@gmail.com>
2026-04-21 11:56:50 +0100

    Kredia Application

---
+++ backend/src/main/java/com/kredia/KrediaApplication.java
@@ -0,0 +1,16 @@
+package com.kredia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableAsync
+@EnableScheduling
+public class KrediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KrediaApplication.class, args);
+    }
+}
